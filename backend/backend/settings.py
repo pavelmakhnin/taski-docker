31c4ce3f06ac4876f77a0c075c5d99c4304af9a7
Pavel Makhnin <p.makhnin@gmail.com>
2023-07-11 23:32:18 +0300

    debug true

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,8 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-# debug to false
-DEBUG = False
+# debug to true
+DEBUG = True
 
 ALLOWED_HOSTS = '158.160.11.193'
 
