9f42c4d1a4c02baf48d6502e43c7840fa0aa89a3
Pavel Makhnin <p.makhnin@gmail.com>
2023-07-11 23:45:00 +0300

    hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 # debug to true
 DEBUG = True
 
-ALLOWED_HOSTS = '158.160.11.193'
-
+# ALLOWED_HOSTS = '158.160.11.193, 127.0.0.1'
+ALLOWED_HOSTS = '158.160.11.193, 127.0.0.1, localhost, kittygrampm.ddns.net'
 
 # Application definition
 
