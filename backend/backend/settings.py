ffcf1902bb6880b4b7142d4a600ba65d09a6611d
Pavel Makhnin <p.makhnin@gmail.com>
2023-07-12 12:16:08 +0300

    allowed_hosts to .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 # ALLOWED_HOSTS = '158.160.11.193, 127.0.0.1' copy
-ALLOWED_HOSTS = ['158.160.11.193', '127.0.0.1', 'localhost', 'kittygrampm.ddns.net']
+# ALLOWED_HOSTS = ['158.160.11.193', '127.0.0.1', 'localhost', 'kittygrampm.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
 
 # Application definition
 
